d1d2b4fe8dbed3a3134caccfed249c2182f03a37
Cameron Green <cameron@camerongreens.com>
2020-04-24 13:50:15 -0500

    scaffolding more

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+# Spotify Control
+
+Disclaimer: This project only runs on a dev machine inside my home. It's not to be used outside your local network, there are many potential security issues if you open this up to the internet
---
+++ index.js
@@ -0,0 +1,18 @@
+window.addEventListener('load', (e) => {
+  console.log('loaded page')
+})
+
+function playpause() {
+  console.log('pause!')
+}
+
+function clickForward() {
+  console.log('clickForward')
+}
+function clickBack() {
+  console.log('clickBack')
+}
+
+function longPoll(params) {
+  console.log('polling...')
+}
